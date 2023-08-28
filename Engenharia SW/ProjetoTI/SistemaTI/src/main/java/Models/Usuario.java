4ccc692934101ae5f9d16cf40c4607536a24f4e7
santosjhony12 <santosdesouzajhony@gmail.com>
2023-08-28 20:43:12 -0300

    Upload classe Pessoa

---
+++ Engenharia SW/ProjetoTI/SistemaTI/src/main/java/Models/Usuario.java
@@ -0,0 +1,12 @@
+package Models;
+
+public class Usuario extends Pessoa{
+    public Usuario(int idPessoa, String nomePessoa, String emailPessoa, String senhaPessoa) {
+        super(idPessoa, nomePessoa, emailPessoa, senhaPessoa);
+    }
+
+    public void criarChamado(Chamado chamado){
+        
+    }
+
+}
